33b9b54e9dde6f5fa54f02e11f6f939589366e47
Luan <luann.victor40@gmail.com>
2021-03-26 00:22:11 -0300

    Criação de search helps Ajuste de Filterbar Funcionalidade de exportar para excel

--- webapp/controller/Report.controller.js
+++ webapp/controller/Report.controller.js
@@ -7,7 +7,7 @@ sap.ui.define([
 	"sap/m/Token",
 	"sap/ui/export/library",
 	"sap/ui/export/Spreadsheet",
-	"sap/ui/model/FilterType"	
+	"sap/ui/model/FilterType"
 ], function(BaseController, ODataModel, Filter, FilterOperator, typeString, Token, exportLibrary, Spreadsheet) {
 	"use strict";
 
@@ -17,12 +17,14 @@ sap.ui.define([
 
 		onInit: function(oEvent) {
 
-			this.oDataServiceUrl = "/sap/opu/odata/sap/ZCOST_CALCULATION_SRV/";
+			this.oDataServiceUrl = "/sap/opu/odata/sap/ZCOST_CALCULATION_SRV";
 			this.oDataModel = new sap.ui.model.odata.ODataModel(this.oDataServiceUrl, true);
 			sap.ui.getCore().setModel(this.oDataModel);
 
-			// this._oMultiInput = this.getView().byId("multiInput");
-
+			// Search Help Matnr
+			this._oMultiInput = this.getView().byId("inputMatnr");
+			this.oColModel = new sap.ui.model.json.JSONModel(sap.ui.require.toUrl("elo/co/lvl/zcostcalc/model") + "/columnsModel.json");
+			// this.getView().setModel(this.oDataModel);
 		},
 
 		onReset: function(oEvent) {
@@ -33,15 +35,19 @@ sap.ui.define([
 		// Funcionando
 		onSearch: function(oEvent) {
 
-			// Busca o valor informado na tela
-			var pMaterial = this.getView().byId("inputMatnr").getValue();
+			// aCols = this.createColumnConfig();
 
-			var filters = new Array();
-			var filterByName = new sap.ui.model.Filter("Matnr", FilterOperator.EQ, pMaterial);
-			filters.push(filterByName);
+			// Busca o valor informado na tela
+			var filters = this.getScreenFilters();
+
+			// var pMaterial = this.getView().byId("inputMatnr").getValue();
+
+			// var filters = new Array();
+			// var filterByName = new sap.ui.model.Filter("Material", FilterOperator.EQ, pMaterial);
+			// filters.push(filterByName);
 
 			// Verifica se os campos estão preenchidos
-			if (this.getView().getController().onCheckFields()) {
+			if (this.checkScreenFilters(filters)) {
 
 				// Chama o serviço passando  filtros
 				var sServiceUrl = "/sap/opu/odata/sap/ZCOST_CALCULATION_SRV";
@@ -58,26 +64,170 @@ sap.ui.define([
 			}
 		},
 
-		onValueHelpRequested: function() {
-			// this._oValueHelpDialog = sap.ui.xmlfragment("elo.co.lvl.zcostcalc.view.ValueHelpDialogConditionsTabOnly", this);
-			// this.getView().addDependent(this._oValueHelpDialog);
-			// this._oValueHelpDialog.setRangeKeyFields([{
-			// 	label: "Material",
-			// 	key: "MatnrId",
-			// 	type: "string",
-			// 	typeInstance: new typeString({}, {
-			// 		maxLength: 7
-			// 	})
-			// }]);
+		getScreenFilters: function() {
 
-			// this._oValueHelpDialog.setTokens(this._oMultiInput.getTokens());
-			// this._oValueHelpDialog.open();
+			var filters = new Array();
+
+			var inputField = this.getView().byId("inputKlvar").getValue();
+			filters.push(new sap.ui.model.Filter("Klvar", FilterOperator.EQ, inputField));
+
+			// inputField = this.getView().byId("inputAmdat").getValue();		
+			// filters.push(new sap.ui.model.Filter("Amdat", FilterOperator.EQ, inputField));  
+
+			inputField = this.getView().byId("inputTvers").getValue();
+			if (inputField !== "") {
+				filters.push(new sap.ui.model.Filter("Tvers", FilterOperator.EQ, inputField));
+			}
+
+			inputField = this.getView().byId("inputMatnr").getValue();
+			filters.push(new sap.ui.model.Filter("Material", FilterOperator.EQ, inputField));
+
+			inputField = this.getView().byId("cboxLvorm").getSelected();
+			if (inputField !== "") {
+				filters.push(new sap.ui.model.Filter("Lvorm", FilterOperator.EQ, inputField));
+			}
+
+			inputField = this.getView().byId("inputWerks").getValue();
+			filters.push(new sap.ui.model.Filter("Plant", FilterOperator.EQ, inputField));
+
+			return filters;
+
+		},
+
+		checkScreenFilters: function(filterTable) {
+
+			// for (var i = 0; i < filterTable.length; i++) {
+
+			// 	if (filterTable[i].sPath === "Plant" ||
+			// 		filterTable[i].sPath === "Material" ||
+			// 		filterTable[i].sPath === "Klvar") {
+
+			// 		if (filterTable[i].oValue1 === "") {
+			// 			sap.m.MessageToast.show("Preencher campos obrigatórios");
+			// 			return false;
+			// 		}
+			// 	}
+			// }
+
+			return true;
+		},
+
+		onChangeDatePicker: function(oEvent) {
+			this.oFilterBar.fireFilterChange(oEvent);
+		},
+
+		onSearchHelpWerks: function(oEvent) {
+			this.inputId = oEvent.getSource().getId();
+			this.oDialog = sap.ui.xmlfragment("elo.co.lvl.zcostcalc.view.fragment.valueHelpWerks", this);
+			var path = "/ZshwerksSet";
+			var oTableStdListTemplate = new sap.m.StandardListItem({
+				title: "{Werks}",
+				description: "{Name1}"
+			});
+			//create a filter for the binding
+			var sInputValue = oEvent.getSource().getValue();
+			var oFilterTableNo = new sap.ui.model.Filter("Werks", FilterOperator.Contains, sInputValue);
+			this.oDialog.unbindAggregation("items");
+			this.oDialog.bindAggregation("items", {
+				path: path,
+				template: oTableStdListTemplate,
+				filters: [oFilterTableNo]
+			});
+			// open value help dialog filtered by the input value
+			this.oDialog.open(sInputValue);
+		},
+
+		handleValueHelpWerksConfirm: function(e) {
+			var s = e.getParameter("selectedItem");
+			if (s) {
+				this.byId(this.inputId).setValue(s.getBindingContext().getObject().Werks);
+				this.readRefresh(e);
+			}
+			this.oDialog.destroy();
+		},
+
+		onSearchHelpKlvar: function(oEvent) {
+			this.inputId = oEvent.getSource().getId();
+			this.oDialog = sap.ui.xmlfragment("elo.co.lvl.zcostcalc.view.fragment.valueHelpKlvar", this);
+			var path = "/ZshklvarSet";
+			var oTableStdListTemplate = new sap.m.StandardListItem({
+				title: "{Klvar}",
+				description: "{Txklv}"
+			});
+			//create a filter for the binding
+			var sInputValue = oEvent.getSource().getValue();
+			var oFilterTableNo = new sap.ui.model.Filter("Klvar", FilterOperator.Contains, sInputValue);
+			this.oDialog.unbindAggregation("items");
+			this.oDialog.bindAggregation("items", {
+				path: path,
+				template: oTableStdListTemplate,
+				filters: [oFilterTableNo]
+			});
+			// open value help dialog filtered by the input value
+			this.oDialog.open(sInputValue);
+		},
+
+		handleValueHelpKlvarConfirm: function(e) {
+			var s = e.getParameter("selectedItem");
+			if (s) {
+				this.byId(this.inputId).setValue(s.getBindingContext().getObject().Klvar);
+				this.readRefresh(e);
+			}
+			this.oDialog.destroy();
+		},
+
+		onSearchHelpMatnr: function() {
+			var aCols = this.oColModel.getData().cols;
+			this._oBasicSearchField = new sap.m.SearchField({
+				visible: false
+			});
+			this._oValueHelpDialog = sap.ui.xmlfragment("elo.co.lvl.zcostcalc.view.fragment.valueHelpMatnr", this);
+			this.getView().addDependent(this._oValueHelpDialog);
+
+			// var matnrLabel = this.getView().getModel("i18n").getResourceBundle().getText("filterMatnr");
+
+			this._oValueHelpDialog.setRangeKeyFields([{
+				label: "Matnr",
+				key: "Matnr",
+				type: "string",
+				typeInstance: new typeString({}, {
+					maxLength: 7
+				})
+			}]);
+
+			var oFilterBar = this._oValueHelpDialog.getFilterBar();
+			oFilterBar.setFilterBarExpanded(false);
+			oFilterBar.setBasicSearch(this._oBasicSearchField);
+
+			this._oValueHelpDialog.getTableAsync().then(function(oTable) {
+				oTable.setModel(this.oDataModel);
+				oTable.setModel(this.oColModel, "columns");
+
+				if (oTable.bindRows) {
+					oTable.bindAggregation("rows", "/ZmatnrSet");
+				}
+
+				// if (oTable.bindItems) {
+				// 	oTable.bindAggregation("items", "/ZmatnrSet", function () {
+				// 		return new sap.m.ColumnListItem({
+				// 			cells: aCols.map(function (column) {
+				// 				return new sap.m.Label({ text: "{" + column.template + "}" });
+				// 			})
+				// 		});
+				// 	});
+				// }
+
+				this._oValueHelpDialog.update();
+			}.bind(this));
+
+			this._oValueHelpDialog.setTokens(this._oMultiInput.getTokens());
+			this._oValueHelpDialog.open();
 		},
 
 		onValueHelpOkPress: function(oEvent) {
-			// var aTokens = oEvent.getParameter("tokens");
-			// this._oMultiInput.setTokens(aTokens);
-			// this._oValueHelpDialog.close();
+			var aTokens = oEvent.getParameter("tokens");
+			this._oMultiInput.setTokens(aTokens);
+			this._oValueHelpDialog.close();
 		},
 
 		onValueHelpCancelPress: function() {
@@ -88,54 +238,99 @@ sap.ui.define([
 			this._oValueHelpDialog.destroy();
 		},
 
-		onSearchHelpMatnr: function(oEvent) {
-			// var sInputValue = oEvent.getSource().getValue();
-			// this.inputId = oEvent.getSource().getId();
-			// var path;
-			// var oTableStdListTemplate;
-			// var oFilterTableNo;
-			// this.oDialog = sap.ui.xmlfragment("elo.co.lvl.zcostcalc.view.valueHelpTable", this);
-			// path = "/ZmatnrSet";
-			// oTableStdListTemplate = new sap.m.StandardListItem({
-			// 	title: "{Matnr}",
-			// 	description: "{Matnr}"
-			// }); // //create a filter for the binding
-			// oFilterTableNo = new sap.ui.model.Filter("Matnr", FilterOperator.Contains, sInputValue);
-			// this.oDialog.unbindAggregation("items");
-			// this.oDialog.bindAggregation("items", {
-			// 	path: path,
-			// 	template: oTableStdListTemplate,
-			// 	filters: [oFilterTableNo]
-			// }); // }// open value help dialog filtered by the input value
-			// this.oDialog.open(sInputValue);
+		onFilterBarSearch: function(oEvent) {
+			var sSearchQuery = this._oBasicSearchField.getValue(),
+				aSelectionSet = oEvent.getParameter("selectionSet");
+			var aFilters = aSelectionSet.reduce(function(aResult, oControl) {
+				if (oControl.getValue()) {
+					aResult.push(new Filter({
+						path: oControl.getName(),
+						operator: FilterOperator.Contains,
+						value1: oControl.getValue()
+					}));
+				}
+
+				return aResult;
+			}, []);
+
+			// aFilters.push(new Filter({
+			// 	filters: [
+			// 		new Filter({ path: "Matnr", operator: FilterOperator.Contains, value1: sSearchQuery })
+			// 	],
+			// 	and: false
+			// }));
+
+			this._filterTable(new Filter({
+				filters: aFilters,
+				and: true
+			}));
 		},
 
-		handleTableValueHelpConfirm: function(e) {
-			var s = e.getParameter("selectedItem");
-			if (s) {
-				this.byId(this.inputId).setValue(s.getBindingContext().getObject().Matnr);
-				this.readRefresh(e);
+		_filterTable: function(oFilter) {
+			var oValueHelpDialog = this._oValueHelpDialog;
+
+			oValueHelpDialog.getTableAsync().then(function(oTable) {
+				if (oTable.bindRows) {
+					oTable.getBinding("rows").filter(oFilter);
+				}
+
+				// if (oTable.bindItems) {
+				// 	oTable.getBinding("items").filter(oFilter);
+				// }
+
+				oValueHelpDialog.update();
+			});
+		},
+
+		_onMultiInputValidate: function(oArgs) {
+			if (oArgs.suggestionObject) {
+				var oObject = oArgs.suggestionObject.getBindingContext().getObject(),
+					oToken = new Token();
+
+				oToken.setKey(oObject.Matnr);
+				oToken.setText(oObject.Maktx + " (" + oObject.Matnr + ")");
+				return oToken;
 			}
-			this.oDialog.destroy();
+
+			return null;
 		},
 
-		onCheckFields: function() {
-			// return this.getView().byId("materialSelect").getSelectedItem().getText() === "" ? false : true;
-			return true;
-		},
-
-		createColumnConfig: function() {
+		createColumnConfig: function(oModel) {
 			var aCols = [];
 
-			aCols.push({
-				property: "Matnr",
-				type: EdmType.String
-			});
+			//Obtem o metadata do webservice
+			var serviceMetadata = sap.ui.getCore().getModel(oModel).getServiceMetadata();
+			//Obtem o schema do werbservice
+			var modelSchema = serviceMetadata.dataServices.schema[0];
+			//Obtem o número de entidades do webservice
+			var lengthEntities = modelSchema.entityType.length;
 
-			aCols.push({
-				property: "Maktx",
-				type: EdmType.String
-			});
+			for (var i = 0; i < lengthEntities; i++) {
+
+				//Verifica se é a entidade correta da tabela
+				if (modelSchema.entityType[i].name === "calculated_cost") {
+
+					//Obtem o número de colunas
+					var lengthColumns = modelSchema.entityType[i].property.length;
+					var oColumn = modelSchema.entityType[i].property;
+
+					//Para cada coluna encontrada no webservice cria uma coluna para geração do excel
+					for (var j = 0; j < lengthColumns; j++) {
+						
+						var arrayExtension = oColumn[j].extensions;
+						var extension = arrayExtension.find( function(arrayExtension){ return arrayExtension.name === "label"; } );
+						var labelText = extension.value;
+						aCols.push({
+							property: oColumn[j].name,
+							type: oColumn[j].type,
+							label: labelText
+						});
+						
+					}
+
+				}
+
+			}
 
 			return aCols;
 		},
@@ -143,12 +338,12 @@ sap.ui.define([
 		onExport: function() {
 			var aCols, oRowBinding, oSettings, oSheet, oTable;
 
-			if (!this._oTable) {
-				this._oTable = this.byId("__table0");
-			}
+			// if (!this._oTable) {
+			this._oTable = this.byId("__table0");
+			// }
 
 			oTable = this._oTable;
-			oRowBinding = oTable.getBinding("items");
+			oRowBinding = oTable.getBinding("rows");
 
 			aCols = this.createColumnConfig();
 
